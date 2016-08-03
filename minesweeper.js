f3078c4de018e6180aa3388e0c8397ddeda28556
Your Name <nickshanks7@gmail.com>
2016-08-03 15:42:10 +1200

    added loops

--- minesweeper.js
+++ minesweeper.js
@@ -103,15 +103,14 @@ var board = {
 }
 
 function startGame () {
-  var board.cells = document.getElementsByClassName('board')[0].children
+  // var board.cells = document.getElementsByClassName('board')[0].children
     for(var i = 0; i < board.cells.length; i++) {
       board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
   }
-}
+
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
 }
-
 // Define this function to look for a win condition:
 //
 // 1. Are all of the cells that are NOT mines visible?
@@ -138,7 +137,7 @@ function countSurroundingMines (cell) {
   var count = 0
 
     for(var j = 0; j < surrounding.length; j++) {
-      if(surrounding[i].isMine === true){
+      if(surrounding[j].isMine){
         count +=1
       }
     }
